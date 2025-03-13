3c8d23f80e69fb4a7f7c76fdb9281cc3c40b454f
Khisab Kurniawan <khisabkurniawan@outlook.com>
2025-03-13 12:57:02 +0700

    Init Released

---
+++ src/app/_meta.js
@@ -0,0 +1,17 @@
+export default {
+  index: "Introduction",
+  stories: "Stories",
+  "*": {
+    theme: {
+      footer: false,
+      breadcrumb: false,
+      typesetting: "article",
+      pagination: false,
+    },
+  },
+  contact: {
+    title: "Forum",
+    type: "page",
+    href: "https://arknesia.com",
+  },
+};
---
+++ src/app/layout.jsx
@@ -0,0 +1,41 @@
+import { Footer, Layout, Navbar } from 'nextra-theme-docs'
+import { Head } from 'nextra/components'
+import { getPageMap } from 'nextra/page-map'
+import 'nextra-theme-docs/style.css'
+ 
+export const metadata = {
+  description: 'Official Arknesia World',
+  title: { template: '%s | Official Arknesia World' },
+  metadataBase: new URL('https://arknesia.world'),
+}
+
+export default async function RootLayout({ children }) {
+  return (
+    <html lang="en" dir="ltr" suppressHydrationWarning>
+      <Head>
+        {/* Your Additional Tags */}
+      </Head>
+      <body>
+        <Layout
+          navbar={
+            <Navbar
+              logo={<span style={{ fontWeight: 800 }}>Arknesia World</span>}
+              chatLink="https://discord.gg/nqbNxtw5QZ"
+            />
+          }
+          pageMap={await getPageMap()}
+          docsRepositoryBase="https://github.com/khisabkurniawn/arknesia-world/tree/main"
+          nextThemes={{ disableTransitionOnChange: false, defaultTheme: 'dark' }}
+          navigation={{ prev: false, next: false }}
+          sidebar={{ toggleButton: false }}
+          footer={<Footer />}
+          darkMode={false}
+          editLink={null}
+          feedback={{ content: null }}
+        >
+          {children}
+        </Layout>
+      </body>
+    </html>
+  )
+}
